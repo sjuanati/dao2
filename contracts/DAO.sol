88b87224f75ca4637da60b91170ec74cb1a4c914
Sergi Juanati <wint3rmute@gmail.com>
2021-01-15 18:15:59 +0100

    Added deposit, withdraw & vote

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -16,10 +16,68 @@ contract DAO {
     }
 
     mapping(bytes32 => Proposal) public proposals; // hash -> proposal
-    mapping(address => mapping(bytes32 => bool)) public votes; // address investor -> proposal -> voted?
+    mapping(address => mapping(bytes32 => bool)) public votes; // address investor -> proposal hash -> voted?
     mapping(address => uint256) public shares; // address investor -> shares
     uint256 public totalShares;
     IERC20 public token;
     uint256 constant CREATE_PROPOSAL_MIN_SHARE = 1000 * 10**18; // minimum number of shares to create a proposal (1000 governance tokens)
     uint256 constant VOTING_PERIOD = 7 days;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    // deposit governance tokens
+    function deposit(uint256 amount) external {
+        shares[msg.sender] += amount;
+        totalShares += amount;
+        token.transferFrom(msg.sender, address(this), amount);
+    }
+
+    // withdraw governance tokens
+    function withdraw(uint256 amount) external {
+        require(shares[msg.sender] >= amount, "not enough shares");
+        shares[msg.sender] -= amount;
+        totalShares -= amount;
+        token.transfer(msg.sender, amount);
+    }
+
+    function createProposal(bytes32 proposalHash) external {
+        require(
+            shares[msg.sender] >= CREATE_PROPOSAL_MIN_SHARE,
+            "not enough shares to create proposal"
+        );
+        require(
+            proposals[proposalHash].hash == bytes32(0),
+            "proposal already exists"
+        );
+        proposals[proposalHash] = Proposal(
+            msg.sender,
+            proposalHash,
+            block.timestamp,
+            0,
+            0,
+            Status.Undecided
+        );
+    }
+
+    function vote(bytes32 proposalHash, Side side) external {
+        Proposal storage proposal = proposals[proposalHash];
+        require(votes[msg.sender][proposalHash] == false, 'already voted');
+        require(proposals[proposalHash].hash != bytes32(0), 'proposal already exist');
+        require(block.timestamp <= proposal.createdAt + VOTING_PERIOD, 'voting period over');
+        votes[msg.sender][proposalHash] = true;
+        if (side == Side.Yes) {
+            proposal.votesYes += shares[msg.sender];
+            // if > 50%
+            if (proposal.votesYes * 100 / totalShares > 50) {
+                proposal.status = Status.Approved;
+            }
+        } else {
+            proposal.votesNo += shares[msg.sender];
+            if (proposal.votesNo * 100 / totalShares > 50) {
+                proposal.status = Status.Rejected;
+            }
+        }
+    }
 }
